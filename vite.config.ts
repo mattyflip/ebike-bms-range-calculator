564b3532ec2f67aa28ae2490bc3d15bd81c8365f
mattyflip <mattyflipdesigns@gmail.com>
2026-05-19 00:59:57 -0400

    Bypass strict server types in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-export default defineConfig(({ command }) => {
-  const isDev = command === 'serve'
-  
-  return {
-    plugins: [
-      react(),
-      ...(isDev ? [basicSsl()] : [])
-    ],
-    server: isDev ? {
-      https: true,
-      host: true
-    } : undefined
-  }
-})
+// https://vite.dev/config/
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    ...(command === 'serve' ? [basicSsl()] : [])
+  ],
+  server: command === 'serve' ? {
+    https: true as any,
+    host: true
+  } : undefined
+}))
