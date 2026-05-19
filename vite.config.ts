8d56b28ac3a55440b6b24ab2e8d317e5d1d3fd37
mattyflip <mattyflipdesigns@gmail.com>
2026-05-19 00:56:41 -0400

    Fix syntax errors in vite.config.ts for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-﻿import { defineConfig } from `"vite`"
-import react from `"@vitejs/plugin-react`"
-import basicSsl from `"@vitejs/plugin-basic-ssl`"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig(({ command }) => ({
   plugins: [
     react(),
-    command === `"serve`" ? basicSsl() : []
+    command === 'serve' ? basicSsl() : []
   ],
   server: {
-    https: command === `"serve`" ,
+    https: command === 'serve',
     host: true
   }
 }))
